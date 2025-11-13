66b4d2c470ec135d2e109981dd189b810ce32871
Antonio Codrean <antonio622004@yahoo.com>
2025-11-13 22:32:07 +0100

    loggs angepasst

--- src/main/java/com/inventory/App.java
+++ src/main/java/com/inventory/App.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.amqp.core.Queue;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 
@@ -15,11 +14,6 @@ import com.inventory.handler.InventoryHandler;
 public class App {
 
     private static final Logger logger = LoggerFactory.getLogger(App.class);
-
-    @Bean
-    public Queue logQueue() {
-        return new Queue("log.queue", true);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
